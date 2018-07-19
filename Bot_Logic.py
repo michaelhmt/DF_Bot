ddf7eb1f0a80b12c2595e5229738b504b3207390
IOKHEIRA\Salem <hindleymht@gmail.com>
2018-07-19 22:43:31 +0100

    finished bot logic script

--- Bot_Logic.py
+++ Bot_Logic.py
@@ -21,5 +21,140 @@ class Game:
     	self.controller.left_mouse_click()
 
     	time.sleep(0.5)
-    	
+
+    	def find_Char(self): 
+    		return self.can_see_Object('Char_Select')
+
+    	def Menu-Quest(self): 
+    		return self.can_see_Object('Quest-Strat_01')
+
+    	def Select_Quest(self): 
+    		return self.can_see_Object('Quest-Selection')
+
+    	def Quest-Enter(self): 
+    		return self.can_see_Object('Quest-Enter')
+
+    	def Convo_Click-Zone(self): 
+    		return self.can_see_Object('Convo_Click-Zone')
+
+    	def Inactive17(self): 
+    		return self.can_see_Object('17_Inactive')
+
+    	def Inactive16(self): 
+    		return self.can_see_Object('16_Inactive')
+
+    	def Inactive15(self): 
+    		return self.can_see_Object('15_Inactive')
+
+    	def Inactive14(self): 
+    		return self.can_see_Object('14_Inactive')
+
+    	def Inactive13(self): 
+    		return self.can_see_Object('13_Inactive')
+
+    	def Inactive12(self): 
+    		return self.can_see_Object('12_Inactive')
+
+    	def Inactive11(self): 
+    		return self.can_see_Object('11_Inactive')
+
+    	def Inactive10(self): 
+    		return self.can_see_Object('10_Inactive')
+
+    	def Inactive09(self): 
+    		return self.can_see_Object('09_Inactive')
+
+    	def Inactive08(self): 
+    		return self.can_see_Object('08_Inactive')
+
+    	def Inactive07(self): 
+    		return self.can_see_Object('07_Inactive')
+
+    	def Inactive06(self): 
+    		return self.can_see_Object('06_Inactive')
+
+    	def Inactive05(self): 
+    		return self.can_see_Object('05_Inactive')
+
+    	def Inactive04(self): 
+    		return self.can_see_Object('04_Inactive')
+
+    	def Inactive03(self): 
+    		return self.can_see_Object('03_Inactive')
+
+    	def Inactive02(self): 
+    		return self.can_see_Object('02_Inactive')
+
+    	def Inactive01(self): 
+    		return self.can_see_Object('01_Inactive')
+
+    	def Active17(self): 
+    		return self.can_see_Object('17-Active')
+
+    	def Active16(self): 
+    		return self.can_see_Object('16-Active')
+
+    	def Active15(self): 
+    		return self.can_see_Object('15-Active')
+
+    	def Active14(self): 
+    		return self.can_see_Object('14-Active')
+
+    	def Active13(self): 
+    		return self.can_see_Object('13-Active')
+
+    	def Active12(self): 
+    		return self.can_see_Object('12-Active')
+
+    	def Active11(self): 
+    		return self.can_see_Object('11-Active')
+
+    	def Active10(self): 
+    		return self.can_see_Object('10-Active')
+
+    	def Active09(self): 
+    		return self.can_see_Object('09-Active')
+
+    	def Active08(self): 
+    		return self.can_see_Object('08-Active')
+
+    	def Active07(self): 
+    		return self.can_see_Object('07-Active')
+
+    	def Active06(self): 
+    		return self.can_see_Object('06-Active')
+
+    	def Active05(self): 
+    		return self.can_see_Object('05-Active')
+
+    	def Active04(self): 
+    		return self.can_see_Object('04-Active')
+
+    	def Active03(self): 
+    		return self.can_see_Object('03-Active')
+
+    	def Active02(self): 
+    		return self.can_see_Object('02-Active')
+
+    	def Active01(self): 
+    		return self.can_see_Object('01-Active')
+
+    	def Finish_Qeust(self): 
+    		return self.can_see_Object('Finish_Qeust')
+
+    	def Close_Loot(self): 
+    		return self.can_see_Object('Close_Loot')
+
+    	def keep(self): 
+    		return self.can_see_Object('keep')
+    		
+    	def Screen_Transition(self): 
+    		return self.can_see_Object('Screen_Transition')
+
+
+
+
+
+
+
 
